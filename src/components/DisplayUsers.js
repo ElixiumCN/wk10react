058690b6fd7de25ba48b3404dd6e3db14cfc8493
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-09-12 17:20:51 +0100

    .
    .

---
+++ src/components/DisplayUsers.js
@@ -0,0 +1,21 @@
+import React from 'react' 
+import { useState } from "react";
+
+//TODO: IMPORT DISPLAY USERS BELOW
+
+
+const DisplayUsers = () => {
+
+    const submitHandler = async (event) => {
+        event.preventDefault()
+        //TODO: CALL DISPLAY USERS FUNCTION
+    }
+
+    return (
+        <form onSubmit ={submitHandler}>
+            <button type='submit'>Click here to display all the users</button>
+        </form>
+    )
+}
+
+export default DisplayUsers
